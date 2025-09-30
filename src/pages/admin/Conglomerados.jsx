b29895a5836f4e3fb33ccdbfb5c625094b5cbce2
Carlos <martin5435412@gmail.com>
2025-09-30 00:35:09 -0500

    .

--- src/pages/admin/Conglomerados.jsx
+++ src/pages/admin/Conglomerados.jsx
@@ -3,6 +3,9 @@ import "../../styles/Home.css";
 const Conglomerados = () => {
   return (
     <>
+    <hr />
+    <hr />
+    
     <h1>Conglomerados</h1>
     <p>ConglomeradosConglomeradosConglomerados</p>
     </>
