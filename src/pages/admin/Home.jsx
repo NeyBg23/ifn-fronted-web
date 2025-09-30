9b3e9b946fccd211e675067a094609f6e10b76bc
Carlos <martin5435412@gmail.com>
2025-09-30 00:06:09 -0500

    Update Home.jsx

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="offcanvas-body">
+            <div className="offcanvas-body custom-navbar">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
                   <button
