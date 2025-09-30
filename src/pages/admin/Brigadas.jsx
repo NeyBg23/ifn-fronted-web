cf8c01eb2b562de1bda5db5994a2275a41d7a8f9
Carlos <martin5435412@gmail.com>
2025-09-30 00:41:53 -0500

    Update Brigadas.jsx

--- src/pages/admin/Brigadas.jsx
+++ src/pages/admin/Brigadas.jsx
@@ -5,6 +5,17 @@ const Brigadas = () => {
     <>
     <h1>Brigadas</h1>
     <p>Aquí puedes gestionar las brigadas registradas.</p>
+
+    <br />
+
+    <div class="card" style="width: 18rem;">
+        <img src="asdas" class="card-img-top" alt="imagen_empleado"/>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
     </>
   );
 };
