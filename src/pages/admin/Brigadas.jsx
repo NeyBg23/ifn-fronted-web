829e90f32409086c021e3a2f7fea421e2450b5e0
Carlos <martin5435412@gmail.com>
2025-09-30 00:50:21 -0500

    Update Brigadas.jsx

--- src/pages/admin/Brigadas.jsx
+++ src/pages/admin/Brigadas.jsx
@@ -1,23 +1,31 @@
+import { useEffect } from "react";
 import "../../styles/Home.css";
 
 const Brigadas = () => {
-  return (
-    <>
-    <h1>Brigadas</h1>
-    <p>Aquí puedes gestionar las brigadas registradas.</p>
 
-    <br />
+    const fetchBrigadas = () => {
+        for (let i; i < 5; i++) {
+            <div className="card" style={{ width: "18rem" }}>
+                <img src="https://cdn.pixabay.com/photo/2023/03/16/08/42/camping-7856198_640.jpg" class="card-img-top" alt="imagen_empleado"/>
+                <div class="card-body">
+                    <h5 class="card-title">Brigada Tigre</h5>
+                    <p class="card-text">Una breve descripción de la brigada.</p>
+                    <a href="#" class="btn btn-primary">Ver Brigada</a>
+                </div>
+            </div>
+        }
+    };
 
-    <div className="card" style={{ width: "18rem" }}>
-        <img src="https://cdn.pixabay.com/photo/2023/03/16/08/42/camping-7856198_640.jpg" class="card-img-top" alt="imagen_empleado"/>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    </>
-  );
+    return (
+        <>
+        <h1>Brigadas</h1>
+        <p>Aquí puedes gestionar las brigadas registradas.</p>
+
+        <br />
+
+        {fetchBrigadas()}
+        </>
+    );
 };
 
 export default Brigadas;
