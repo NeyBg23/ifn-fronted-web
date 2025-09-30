588ff940b94a0c6026c5214342f01ec650b1d5b2
Carlos <martin5435412@gmail.com>
2025-09-29 23:16:57 -0500

    Update Home.jsx

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -3,82 +3,51 @@ import '../../styles/Home.css'; // Importa los estilos CSS para el componente Lo
 const Home = () => {
     return (
         <div className="home-container">
-        <nav className="navbar navbar-dark custom-navbar fixed-top">
-            <div className="container-fluid">
-            <a className="navbar-toggler" href="#">Inventario Forestal Nacional</a>
-            <button
-                className="navbar-brand"
-                type="button"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasDarkNavbar"
-                aria-controls="offcanvasDarkNavbar"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            {/* Sidebar offcanvas desde la izquierda */}
-            <div
-                className="offcanvas offcanvas-start text-bg-dark custom-navbar"
-                tabIndex={-1}
-                id="offcanvasDarkNavbar"
-                aria-labelledby="offcanvasDarkNavbarLabel"
-            >
-                <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                    Menú Administrador
-                </h5>
-                <button
-                    type="button"
-                    className="btn-close btn-close-white"
-                    data-bs-dismiss="offcanvas"
-                    aria-label="Close"
-                ></button>
+            <nav class="navbar navbar-dark custom-navbar fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Inventario Forestal Nacional</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end custom-navbar text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu Administrador</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-
-                <div className="offcanvas-body">
-                <ul className="navbar-nav flex-column pe-3">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
-                        Home
-                    </a>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Brigadas</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Brigadas</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Conglomerados</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Conglomerados</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Empleados</a>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Perfil</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Perfil</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider"></hr>
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                     </li>
-                    <li className="nav-item dropdown">
-                    <a
-                        className="nav-link dropdown-toggle"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Más opciones
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-dark">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" href="#">Something else</a></li>
                     </ul>
-                    </li>
-                </ul>
+                </div>
                 </div>
             </div>
-            </div>
-        </nav>
+            </nav>
         </div>
-
     );
 };
 
