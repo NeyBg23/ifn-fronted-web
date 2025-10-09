acdf43dff308eff4b52d95ae8a1c2dc9f4d2a47f
Carlos <martin5435412@gmail.com>
2025-10-08 23:19:42 -0500

    .

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js' //se importa la libreria de supabase
 
 // Cliente para Brigadas
-const brigadasUrl = import.meta.env.VITE_SUPABASE_BRIGADAS_URL // Variable de entorno para la URL de Supabase de Brigadas
+const brigadasUrl = import.meta.env.VITE_SUPABASE_URL_BRIGADAS // Variable de entorno para la URL de Supabase de Brigadas
 const brigadasAnonKey = import.meta.env.VITE_SUPABASE_BRIGADAS_ANON_KEY // Variable de entorno para la clave anónima de Supabase de Brigadas
 export const supabaseBrigadas = createClient(brigadasUrl, brigadasAnonKey) // Crear el cliente de Supabase para Brigadas
 
