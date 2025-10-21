ad2adc085139e6552ae4b31ffbb1ad1d5614cb71
NeyBg23 <mathicuenta23@gmail.com>
2025-10-21 00:10:39 -0500

    boostarp
    cargandolo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react' // ← Importa StrictMode para ayudar a detectar problemas en la aplicación
 import { createRoot } from 'react-dom/client' // ← Importa createRoot para renderizar la aplicación en el DOM
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
