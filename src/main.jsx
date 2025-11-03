35ebf2bd6fb7928f0e0b9077d49eb8fd4f833816
NeyBg23 <mathicuenta23@gmail.com>
2025-11-02 22:48:00 -0500

    Update main.jsx
    Corregir import de AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import { StrictMode } from 'react' // ← Importa StrictMode para ayudar a detectar problemas en la aplicación
 import { createRoot } from 'react-dom/client' // ← Importa createRoot para renderizar la aplicación en el DOM
-import { AuthProvider } from './context/AuthProvider' // ← Importa el proveedor de autenticación // ← Asegura que toda la app tenga acceso al contexto de autenticación // este lo cree yo cuando hice el archivo useAuth.jsx
+import { AuthProvider } from './hooks/useAuth.jsx';// ← Importa el proveedor de autenticación // ← Asegura que toda la app tenga acceso al contexto de autenticación // este lo cree yo cuando hice el archivo useAuth.jsx
 import App from './App.jsx'
 
 
