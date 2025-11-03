a47a767c7da3e82bdc233659a3ee07394f13935a
NeyBg23 <mathicuenta23@gmail.com>
2025-11-02 22:34:38 -0500

    importar el provedor de autenticacion de react
    Asegura que toda la app tenga acceso al contexto de autenticación // este lo cree yo cuando hice el archivo useAuth.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
+// src/main.jsx
 import { StrictMode } from 'react' // ← Importa StrictMode para ayudar a detectar problemas en la aplicación
 import { createRoot } from 'react-dom/client' // ← Importa createRoot para renderizar la aplicación en el DOM
+import { AuthProvider } from './context/AuthProvider' // ← Importa el proveedor de autenticación // ← Asegura que toda la app tenga acceso al contexto de autenticación // este lo cree yo cuando hice el archivo useAuth.jsx
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* AuthProvider envuelve toda la app para proporcionar contexto de autenticación */}
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
