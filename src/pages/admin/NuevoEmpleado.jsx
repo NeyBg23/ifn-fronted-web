d77a27a943a1c4a32a1dc3344073e9254c790897
Carlos <martin5435412@gmail.com>
2025-11-15 01:38:48 -0500

    Update NuevoEmpleado.jsx

--- src/pages/admin/NuevoEmpleado.jsx
+++ src/pages/admin/NuevoEmpleado.jsx
@@ -387,10 +387,8 @@ const NuevoEmpleado = () => {
       if (!nuevoEmpleado.cargo || !nuevoEmpleado.fecha_ingreso || !nuevoEmpleado.rol) {
         setMostrarErrorCamposVacios(true)
         isValid = false;
-      }
+      } else return document.getElementById("formNuevoEmpleado").requestSubmit();
     }
-
-    if (currentStep === totalSteps) return handleCrearEmpleado();
     
     // Si las validaciones pasan, avanza
     if (isValid && currentStep < totalSteps) {
